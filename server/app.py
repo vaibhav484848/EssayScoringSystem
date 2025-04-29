69be775c5054b7691820a92af08ecdc13a104d68
vin6819 <phantomera.2000@gmail.com>
2025-04-29 16:37:58 +0530

    backend updated

--- server/app.py
+++ server/app.py
@@ -7,12 +7,15 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from tensorflow.keras.preprocessing.text import Tokenizer
 import nltk
+from happytransformer import HappyTextToText, TTSettings
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('punkt_tab')
 
 app = Flask(__name__)
 CORS(app)
+happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
+args = TTSettings(num_beams=5, min_length=1)
 
 @app.route('/score', methods=['POST'])
 def score():
@@ -20,13 +23,13 @@ def score():
     print(text)
     res = get_score(text)
     print(res)
-    return jsonify(round(res['prediction'][0][0]))
+    return jsonify({"score": round(res['prediction'][0][0]), "correct_text": res['correct_text']})
 
 def get_score(text):
     # Load the model and vectorizer
     with open('model.pkl', 'rb') as f:
         model = pickle.load(f)
-
+    correct_text = happy_tt.generate_text(text, args=args)
     text = clean_essay(text)
     text = remove_stopwords(text)
     text = remove_punc(text)
@@ -38,7 +41,7 @@ def get_score(text):
     # Get the prediction
     prediction = model.predict(np.array(padded))
 
-    return {'prediction': prediction}
+    return {'prediction': prediction, 'correct_text': correct_text.text}
 
 
 def clean_essay(essay):
